c128bf0d013e448bdfdc8c6e7c44b2cd3f27eace
DenPlayTS <tishakovdenis2021@gmail.com>
2025-05-04 12:07:30 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -79,3 +79,5 @@ $('.container').on('touchend', function(event) {
         backward()
     }
 })
+
+
---
+++ slick.js
@@ -0,0 +1,9 @@
+$('.carousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    prevArrow: '<img src="images/arrow.png">',
+    nextArrow: '<img src="images/arrow_right.png">'
+});
